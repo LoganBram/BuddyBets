b33e4d830ace7cc56fbe93b2ee04750b7655d0ef
Logan Bramwell <loganbramwellqu@gmail.com>
2023-07-20 16:45:19 -0700

    tester file for smaller things and axios

---
+++ tester.js
@@ -0,0 +1,21 @@
+// Get the current date
+const currentDate = new Date();
+
+// Function to format a date as "yyyy-mm-dd"
+function formatDate(date) {
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const day = String(date.getDate()).padStart(2, "0");
+  return `${year}-${month}-${day}`;
+}
+
+// Calculate the next 7 days
+const next7Days = [];
+for (let i = 0; i < 15; i++) {
+  const nextDate = new Date();
+  nextDate.setDate(currentDate.getDate() + i + 1);
+  next7Days.push(formatDate(nextDate));
+}
+
+console.log("Current Date:", formatDate(currentDate));
+console.log("Next 7 Days:", next7Days);
