6911c8ae702f474609155265caedeed12ad44767
Logan Bramwell <loganbramwellqu@gmail.com>
2023-07-20 16:44:30 -0700

    game update function for hte next 7 day working, returns the dates so i could make sure they stayed in order with promise.all

--- backend/modules/datafetch.js
+++ backend/modules/datafetch.js
@@ -1,3 +1,41 @@
+const axios = require("axios");
 const pool = require("../db");
+const { next7days } = require("./dates");
 
-const fetch = async (req, res) => {};
+const updateGames = async () => {
+  try {
+    const thisweek = await next7days();
+
+    //calls the api to get the games for each day in the week
+    //api only allows calling the games for individual days but not for a range of days
+    //this is required otherwise the display on front page would only be one day at a time
+    const apiCalls = thisweek.map((date) => {
+      const options = {
+        method: "GET",
+        url: `https://api-basketball.p.rapidapi.com/games?date=${date}&timezone=canada/vancouver`,
+        headers: {
+          "x-rapidapi-host": "api-basketball.p.rapidapi.com",
+          "x-rapidapi-key":
+            "17c1c2bc80msh0edbcbc5e8d8cbap178151jsnb0abea9d94a3",
+        },
+      };
+      return axios(options);
+    });
+
+    //handles all the promises from the api call
+    const responses = await Promise.all(apiCalls);
+    //once all promises are fulfilled,  we obtain the game dates
+    //this process is to keep the dates in order
+    for (const response of responses) {
+      const apiResponse = response.data;
+      const gameDates = apiResponse.response.map((game) => game.date);
+      console.log(gameDates);
+      console.log("break");
+      console.log("");
+    }
+  } catch (error) {
+    console.error("Error fetching games:", error.message);
+  }
+};
+
+updateGames();
---
+++ backend/modules/dates.js
@@ -0,0 +1,30 @@
+var request = require("request");
+// Get the current date
+
+const next7days = async () => {
+  const currentDate = new Date();
+
+  // Function to format a date as "yyyy-mm-dd"
+  function formatDate(date) {
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, "0");
+    const day = String(date.getDate()).padStart(2, "0");
+    return `${year}-${month}-${day}`;
+  }
+
+  // Calculate the next 7 days
+  const next7Days = [];
+  for (let i = 0; i < 7; i++) {
+    const nextDate = new Date();
+    nextDate.setDate(currentDate.getDate() + i + 1);
+    next7Days.push(formatDate(nextDate));
+  }
+
+  console.log("Current Date:", formatDate(currentDate));
+  console.log("Next 7 Days:", next7Days);
+  return next7Days;
+};
+
+module.exports = {
+  next7days,
+};
