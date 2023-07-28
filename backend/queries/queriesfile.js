451d681bdd9999ef7eb9e084beeb6db6f8f7212e
Logan Bramwell <loganbramwellqu@gmail.com>
2023-07-28 12:44:42 -0700

    added query for getting ids for date

--- backend/queries/queriesfile.js
+++ backend/queries/queriesfile.js
@@ -3,9 +3,12 @@ const addGames =
   " INSERT INTO games (gameid, startdate, homeid, awayid, time ) VALUES ($1, $2, $3, $4, $5 )";
 const updateDayScores =
   "UPDATE games SET homescore = $1, awayscore = $2 WHERE gameid = $3";
+const getGameidForDay =
+  "SELECT gameid FROM games WHERE startdate::text LIKE $1";
 
 module.exports = {
   getAllTest,
   addGames,
   updateDayScores,
+  getGameidForDay,
 };
