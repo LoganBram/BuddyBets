1a763e29e9dc3d35e63ea38f96b304b64a9dcdb9
Logan Bramwell <loganbramwellqu@gmail.com>
2023-07-31 13:59:54 -0700

    authorization of token middleware, validinfo file for checking if the values are empty

---
+++ backend/middleware/authorization.js
@@ -0,0 +1,21 @@
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+module.exports = async (req, res, next) => {
+  try {
+    //assigns jwtTOKEN to the request header labeled token
+    const jwtToken = req.header("token");
+    //if there is no token then throw error
+    if (!jwtToken) {
+      throw error;
+    }
+
+    //checks using env SECRET and token extracted, to verify the user
+    const payload = jwt.verify(jwtToken, process.env.JWTSECRET);
+    //if token valid, extracts user property to req.user
+    req.user = payload.user;
+  } catch (err) {
+    console.error(err.message);
+    return res.status(403).json("Not Authorized");
+  }
+};
