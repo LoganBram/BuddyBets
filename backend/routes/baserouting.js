0832040e4c3fa26602d56c0a8318dd8d131d3ae9
Logan Bramwell <loganbramwellqu@gmail.com>
2023-07-31 15:04:55 -0700

    added validation to router

--- backend/routes/baserouting.js
+++ backend/routes/baserouting.js
@@ -2,6 +2,7 @@ const { Router } = require("express");
 const router = Router();
 const controller = require("../controllers/basecontroller.js");
 const usercontroller = require("../controllers/usercontroller.js");
+const validInfo = require("../middleware/validinfo.js");
 
 router.get("/getgames", controller.getGamesController);
 
@@ -10,8 +11,8 @@ router.get("/getscores", controller.getScoresController);
 router.post("/sendbetrequest", usercontroller.BetReq);
 
 // -> auth/
-router.post("/register", usercontroller.RegisterUser);
+router.post("/register", validInfo, usercontroller.RegisterUser);
 
-router.post("/login", usercontroller.LoginUser);
+router.post("/login", validInfo, usercontroller.LoginUser);
 
 module.exports = router;
