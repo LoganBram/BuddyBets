a7cc9934ba23b314646de1a27f1080072d95b0a0
Logan Bramwell <loganbramwellqu@gmail.com>
2023-07-31 11:22:01 -0700

    deletion, restarting with angular

--- backend/server.js
+++ backend/server.js
@@ -9,13 +9,4 @@ app.set("view engine", "ejs");
 
 app.use("/route", routing);
 
-//rendering for ejs pages, routes will be changed later
-app.get("/", (req, res) => res.render("index"));
-
-app.get("/users/register", (req, res) => res.render("register"));
-
-app.get("/users/login", (req, res) => res.render("login"));
-
-app.get("/users/dashboard", (req, res) => res.render("dashboard"));
-
 app.listen(port, () => console.log("listening on port " + port));
