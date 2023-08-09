38417067669d51841cb5834f0fcfe4a1ea7a25d9
Logan Bramwell <loganbramwellqu@gmail.com>
2023-08-08 17:36:08 -0700

    token in local storage and navigation after login'

--- frontend/angular/src/app/login/login.component.ts
+++ frontend/angular/src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../auth.service';
+import { Router} from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -10,7 +11,7 @@ export class LoginComponent {
 
   response: any = null;
 
-  constructor(private authservice: AuthService){}
+  constructor(private authservice: AuthService, private _route: Router){}
   
   loginUserData = {email: '', password: ''}
 
@@ -18,11 +19,15 @@ export class LoginComponent {
     this.authservice.loginUser(this.loginUserData).subscribe({
       next: (res)=> {
         this.response = res.response;
+        localStorage.setItem('token', res.token)
+        this._route.navigate(['/dashboard'])
       },
       error: (err) => {
-        
+        if (err.status === 401) {
           this.response = err.error; // Tdisplay error message returned from backend
-        
+        } else {
+          this.response = 'An unexpected error occured. Please try again later.';
+        }
       },
     })
 
--- frontend/angular/src/app/register/register.component.ts
+++ frontend/angular/src/app/register/register.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../auth.service';
-import { Input } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -11,13 +11,15 @@ export class RegisterComponent {
   response : any = null;
 
   registerUserData = {name: '', email: '', password: ''};
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private _route: Router) { }
 
   registerUser() {
     this.authService.registerUser(this.registerUserData).subscribe({
       next: (res) => {
         //display success message
         this.response = res.response
+        localStorage.setItem('token', res.token)
+        this._route.navigate(['/dashboard'])
       },
       error: (err) => {
         if (err.status === 401) {
