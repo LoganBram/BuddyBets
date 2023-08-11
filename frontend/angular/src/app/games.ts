4fb3d569cddf7955122c8f9780f485ea9bd8abce
Logan Bramwell <loganbramwellqu@gmail.com>
2023-08-10 20:00:06 -0700

    formats date and time correctly, switched games obj to use string for date and time

--- frontend/angular/src/app/games.ts
+++ frontend/angular/src/app/games.ts
@@ -3,9 +3,9 @@ export interface Game{
     awayteam: string,
     hometeam: string,
     startdate: string,
+    time: string,
     homeid: string,
-    awayid: string,
-    time: Date,
+    awayid: string, 
     homescore?: number | null,
     awayscore?: number | null,
 }
@@ -14,10 +14,10 @@ export const games = [
     {gameid: 1,
     awayteam: "Team 1",
     hometeam: "Team 2",
-    startdate: "2020-12-12",
+    startdate: "2023-08-01 19:00:00-07" ,
+    time: "02:00:00",
     homeid: "2",
     awayid: "3",
-    time: new Date("2020-12-12T12:00:00"),
     homescore: 0,
     awayscore: 0,}
 ]
--- frontend/angular/src/app/gamesdisplay/gamesdisplay.component.ts
+++ frontend/angular/src/app/gamesdisplay/gamesdisplay.component.ts
@@ -8,4 +8,34 @@ import {games} from '../games'
 export class GamesdisplayComponent {
 
   games = [...games]
+
+  //formats date in Tue, Aug 1
+  formatDate(dateString: string): string {
+    const date = new Date(dateString);
+    return date.toLocaleDateString("en-US", {
+      weekday: "short",
+      month: "short",
+      day: "numeric",
+    });
+  }
+
+  //formats time to 6:00 PM
+  formatTime(timeString: string): string {
+    const timeArray = timeString.split(":");
+    const time = new Date(0);
+    time.setUTCHours(Number(timeArray[0]));
+    time.setUTCMinutes(Number(timeArray[1]));
+
+    return time.toLocaleTimeString("en-US", {
+      hour: "numeric",
+      minute: "numeric",
+      hour12: true,
+    });
+  }
 }
+
+
+
+
+
+
